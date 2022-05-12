835a5245a18a6effa831703eaa7dc0d828e6d04f
nayarithzambrano <nayarithzambrano@colegioguanenta.edu.co>
2022-05-12 17:07:38 -0500

    desarrollo ejercicios

---
+++ Ejercicio1/while.py
@@ -0,0 +1,12 @@
+# Bucle While
+
+import math
+
+numero = int(input("Digite un numero: "))
+
+while numero<0:
+    print("Error -> deberia ser un numero positivo")
+    numero = int(input("Digite un numero: "))
+
+
+print(f"\nSu raiz cuadrada es: {(math.sqrt(numero)):2f}")
